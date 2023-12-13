a7f2752791ce07b10403b6956c1a1138c6ef9ebb
Marcelo Módolo <marcelo.rmodolo@gmail.com>
2023-12-13 10:48:26 -0300

    Permitir rodar a aplicação local ou em uma instância EC2

--- src/main.py
+++ src/main.py
@@ -1,18 +1,38 @@
+import logging
 import os
+import logging
 from dotenv import load_dotenv
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("../tmp/check_aws_resources.log"),
+        logging.StreamHandler()
+    ]
+)
 
 
 load_dotenv()
 
-ACCOUNT_ID = os.getenv('ACCOUNT_ID')
-APPLICATION_NAME = os.getenv('APPLICATION_NAME')
-ASSUME_ROLE = os.getenv('ASSUME_ROLE')
-AWS_DEFAULT_PROFILE = os.getenv('AWS_DEFAULT_PROFILE')
-AWS_EB_PROFILE = os.getenv('AWS_EB_PROFILE')
-AWS_PROFILE = os.getenv('AWS_PROFILE')
+# Essas variáveis são carregadas a partir do arquivo '.env' e podem
+# ser sobreescritas com variáveis de ambiente
+ACCOUNT_ID = os.getenv('ACCOUNT_ID') # Conta root na organização
+AWS_DEFAULT_PROFILE = os.getenv('AWS_DEFAULT_PROFILE') # profile que irá assumir a role
+AWS_EB_PROFILE = os.getenv('AWS_EB_PROFILE') # profile que irá assumir a role
+AWS_PROFILE = os.getenv('AWS_PROFILE') # profile que irá assumir a role
 AWS_REGION = os.getenv('AWS_REGION')
-USE_ORG_FOR_ACCOUNT_LIST = os.getenv('USE_ORG_FOR_ACCOUNT_LIST')
+USE_ORG_FOR_ACCOUNT_LIST = os.getenv('USE_ORG_FOR_ACCOUNT_LIST') # Valos listar todas as cotontas na organização
+
 
 def main():
-    from .dependencyChecker import lambda_handler
-    lambda_handler(None, None)
+    from dependencyChecker import lambda_handler
+    # Esse é o entrypoint da função lambda os parâmetros não são usados
+    # na função
+    event = None
+    context = None
+    lambda_handler(event, context)
+
+
+main()
